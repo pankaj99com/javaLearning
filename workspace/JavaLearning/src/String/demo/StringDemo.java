0b80b6384396d6e36ee52f01df429b1382c2a6a4
Pankaj <Pankaj99.com@gmail.com>
2021-09-22 13:30:33 +0530

    Test

--- workspace/JavaLearning/src/String/demo/StringDemo.java
+++ workspace/JavaLearning/src/String/demo/StringDemo.java
@@ -3,29 +3,35 @@ package String.demo;
 public class StringDemo {
 
 	public static void main(String[] args) {
-	String s1="This is Selenium training";
-	String s2="pankaj-kumar-ranchi-jharkhand";
-	
-	boolean result=s1.startsWith("This");
-	System.out.println(result);
-	boolean result1=s1.endsWith("Training");
-	System.out.println(result1);
-	boolean result3=s1.isEmpty();
-	System.out.println(result3);
-	char result4=s1.charAt(2);
-	System.out.println(result4);
-	String result5=s1.substring(5, 9);
-	System.out.println(result5);
-	boolean result6=s1.contains(result5);
-	System.out.println(result6);
-	String[] result7=s2.split("-");
-	for(int i=0;i<result7.length;i++)
-	{
-		System.out.println("results are " + i + " " +result7[i]);
-				
-	}
-	System.out.println(result7);
-	
+		String s1 = "This is Selenium training";
+		String s2 = "pankaj-kumar-ranchi-jharkhand";
+
+		boolean result = s1.startsWith("This");
+		System.out.println(result);
+		
+		boolean result1 = s1.endsWith("Training");
+		System.out.println(result1);
+		
+		boolean result3 = s1.isEmpty();
+		System.out.println(result3);
+		
+		char result4 = s1.charAt(2);
+		System.out.println(result4);
+		
+		String result5 = s1.substring(5, 9);
+		System.out.println(result5);
+		
+		boolean result6 = s1.contains(result5);
+		System.out.println(result6);
+		
+		String[] result7 = s2.split("-");
+		
+		for (int i = 0; i < result7.length; i++) {
+			System.out.println("results are " + i + " " + result7[i]);
+
+		}
+		System.out.println(result7);
+
 	}
 
 }
--- workspace/JavaLearning/src/String1/demo/RemovingDuplicates.java
+++ workspace/JavaLearning/src/String1/demo/RemovingDuplicates.java
@@ -13,17 +13,24 @@ import java.util.Set;
 public class RemovingDuplicates {
 
 	public static void main(String[] args) {
-		String str = "Pankaj";
+		String str = "Pppankaj";
 		System.out.println(RemoveDuplicates(str));
 	}
 
 	public static String RemoveDuplicates(String str) {
+		
 		Set<Character> set = new HashSet<>();
+		
 		StringBuffer sf = new StringBuffer();
+		
 		for (int i = 0; i < str.length(); i++) {
-			Character ch = str.charAt(i);
+			
+			Character ch = str.toLowerCase().charAt(i);
+			
 			if (!set.contains(ch)) {
+				
 				set.add(ch);
+				
 				sf.append(ch);
 			}
 		}
